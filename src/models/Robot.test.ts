e56d0e24ad487040c0f88d195f0a7e1b8bda3029
Michael Roger <michael.roger@ovoenergy.com>
2021-03-06 14:50:31 +0000

    Added lost functionality to robot.

--- src/models/Robot.test.ts
+++ src/models/Robot.test.ts
@@ -1,13 +1,16 @@
-import { Coordinate, MarsMap } from "./MarsMap";
-import { Robot, Orientation, MoveCommand } from "./Robot";
+import { MarsMap } from "./MarsMap";
+import { Robot, MoveCommand, Position } from "./Robot";
 
 describe("Robot", () => {
-  let mockMap: MarsMap;
+  let testMap: MarsMap;
 
   beforeEach(() => {
-    mockMap = ({
-      isCoordinateOnMap: jest.fn(),
-    } as unknown) as MarsMap;
+    testMap = new MarsMap({
+      minX: 0,
+      maxX: 9,
+      minY: 0,
+      maxY: 9,
+    });
   });
 
   afterEach(() => {
@@ -16,32 +19,66 @@ describe("Robot", () => {
 
   describe("status", () => {
     it("should give status of position & orientation", () => {
-      const robot = new Robot({ x: 2, y: 1 }, "N", mockMap);
+      const robot = new Robot({ x: 2, y: 1, orientation: "N" }, testMap);
       expect(robot.status).toEqual("(2, 1, N)");
     });
   });
 
   describe("move", () => {
-    it.each<[Coordinate, Orientation, MoveCommand, string]>([
-      [{ x: 1, y: 3 }, "N", "F", "(1, 4, N)"],
-      [{ x: 1, y: 3 }, "E", "F", "(2, 3, E)"],
-      [{ x: 1, y: 3 }, "S", "F", "(1, 2, S)"],
-      [{ x: 1, y: 3 }, "W", "F", "(0, 3, W)"],
-      [{ x: 1, y: 3 }, "N", "L", "(1, 3, W)"],
-      [{ x: 1, y: 3 }, "E", "L", "(1, 3, N)"],
-      [{ x: 1, y: 3 }, "S", "L", "(1, 3, E)"],
-      [{ x: 1, y: 3 }, "W", "L", "(1, 3, S)"],
-      [{ x: 1, y: 3 }, "N", "R", "(1, 3, E)"],
-      [{ x: 1, y: 3 }, "E", "R", "(1, 3, S)"],
-      [{ x: 1, y: 3 }, "S", "R", "(1, 3, W)"],
-      [{ x: 1, y: 3 }, "W", "R", "(1, 3, N)"],
+    it.each<[Position, MoveCommand, string]>([
+      [{ x: 1, y: 3, orientation: "N" }, "F", "(1, 4, N)"],
+      [{ x: 1, y: 3, orientation: "E" }, "F", "(2, 3, E)"],
+      [{ x: 1, y: 3, orientation: "S" }, "F", "(1, 2, S)"],
+      [{ x: 1, y: 3, orientation: "W" }, "F", "(0, 3, W)"],
+      [{ x: 1, y: 3, orientation: "N" }, "L", "(1, 3, W)"],
+      [{ x: 1, y: 3, orientation: "E" }, "L", "(1, 3, N)"],
+      [{ x: 1, y: 3, orientation: "S" }, "L", "(1, 3, E)"],
+      [{ x: 1, y: 3, orientation: "W" }, "L", "(1, 3, S)"],
+      [{ x: 1, y: 3, orientation: "N" }, "R", "(1, 3, E)"],
+      [{ x: 1, y: 3, orientation: "E" }, "R", "(1, 3, S)"],
+      [{ x: 1, y: 3, orientation: "S" }, "R", "(1, 3, W)"],
+      [{ x: 1, y: 3, orientation: "W" }, "R", "(1, 3, N)"],
     ])(
       "should move the robot to the expected position/orientation (#%#)",
-      (position, orientation, moveCommand, expectedStatus) => {
-        const robot = new Robot(position, orientation, mockMap);
+      (position, moveCommand, expectedStatus) => {
+        const robot = new Robot(position, testMap);
         robot.move(moveCommand);
         expect(robot.status).toEqual(expectedStatus);
       }
     );
+  });
+
+  describe("isLost", () => {
+    it("should return false when current position is on map", () => {
+      jest.spyOn(testMap, "isCoordinateOnMap").mockReturnValue(true);
+      const position: Position = { x: 3, y: 4, orientation: "N" };
+      const robot = new Robot(position, testMap);
+      const result = robot.isLost;
+      expect(result).toEqual(false);
+      expect(testMap.isCoordinateOnMap).toHaveBeenCalledWith(position);
+    });
+
+    it("should return true when current position is not on map", () => {
+      jest.spyOn(testMap, "isCoordinateOnMap").mockReturnValue(false);
+      const position: Position = { x: 3, y: 4, orientation: "N" };
+      const robot = new Robot(position, testMap);
+      const result = robot.isLost;
+      expect(result).toEqual(true);
+      expect(testMap.isCoordinateOnMap).toHaveBeenCalledWith(position);
+    });
+  });
+
+  describe("moves into lost position", () => {
+    it("should give status of last known position when moved into lost position", () => {
+      jest
+        .spyOn(testMap, "isCoordinateOnMap")
+        .mockImplementation((coordinate) => {
+          return coordinate.y >= 0;
+        });
+      const robot = new Robot({ x: 0, y: 0, orientation: "S" }, testMap);
+      robot.move("F");
+      expect(robot.isLost).toEqual(true);
+      expect(robot.status).toEqual("(0, 0, S) LOST");
+    });
   });
 });
--- src/models/Robot.ts
+++ src/models/Robot.ts
@@ -1,21 +1,28 @@
 import { Coordinate, MarsMap } from "./MarsMap";
 
-export type Orientation = "N" | "E" | "S" | "W";
-export type RotateDirection = "L" | "R";
+type Orientation = "N" | "E" | "S" | "W";
+type RotateDirection = "L" | "R";
 export type MoveCommand = "F" | RotateDirection;
+
+export interface Position extends Coordinate {
+  orientation: Orientation;
+}
 
 export class Robot {
   private orientations: Orientation[] = ["N", "E", "S", "W"];
+  private positionLog: Position[];
 
-  constructor(
-    private position: Coordinate,
-    private orientation: Orientation,
-    private map: MarsMap
-  ) {}
+  constructor(position: Position, private map: MarsMap) {
+    this.positionLog = [position];
+  }
 
   public get status(): string {
-    const { x, y } = this.position;
-    return `(${x}, ${y}, ${this.orientation})`;
+    if (this.isLost) {
+      const { x, y, orientation } = this.lastPositionOnMap;
+      return `(${x}, ${y}, ${orientation}) LOST`;
+    }
+    const { x, y, orientation } = this.position;
+    return `(${x}, ${y}, ${orientation})`;
   }
 
   public move(command: MoveCommand) {
@@ -31,22 +38,47 @@ export class Robot {
     }
   }
 
+  public get isLost(): boolean {
+    return !this.map.isCoordinateOnMap(this.position);
+  }
+
+  private get position(): Position {
+    return this.positionLog[this.positionLog.length - 1];
+  }
+
+  private set position(newPosition: Position) {
+    this.positionLog.push(newPosition);
+  }
+
+  private get lastPositionOnMap(): Position {
+    const positionLogReversed = [...this.positionLog].reverse();
+    return (
+      positionLogReversed.find((position) =>
+        this.map.isCoordinateOnMap(position)
+      ) || this.positionLog[0]
+    );
+  }
+
   private moveForward() {
-    const { x, y } = this.position;
+    const { x, y, orientation } = this.position;
     this.position = {
-      x:
-        this.orientation === "E" ? x + 1 : this.orientation === "W" ? x - 1 : x,
-      y:
-        this.orientation === "N" ? y + 1 : this.orientation === "S" ? y - 1 : y,
+      x: orientation === "E" ? x + 1 : orientation === "W" ? x - 1 : x,
+      y: orientation === "N" ? y + 1 : orientation === "S" ? y - 1 : y,
+      orientation,
     };
   }
 
   private rotate(direction: RotateDirection) {
+    const { x, y, orientation } = this.position;
     const indexShift = direction === "L" ? -1 : 1;
-    const currentIndex = this.orientations.indexOf(this.orientation);
+    const currentIndex = this.orientations.indexOf(orientation);
     const newIndex =
       (currentIndex + indexShift + this.orientations.length) %
       this.orientations.length;
-    this.orientation = this.orientations[newIndex];
+    this.position = {
+      x,
+      y,
+      orientation: this.orientations[newIndex],
+    };
   }
 }
